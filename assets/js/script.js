91fd0be0b542985f399cbb4dd34f8bc3bbfd2a2f
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2021-10-15 22:56:55 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
+/*
+    Пароль на странице
+*/
 const password = prompt('Введіть пароль');
 
 if (password == "111") {
@@ -8,4 +11,45 @@ if (password == "111") {
   
       window.location.href = 'https://google.com';
   
+}
+
+/*
+    Поиск по странице
+*/
+var TRange=null;
+
+function findString (str) {
+ if (parseInt(navigator.appVersion)<4) return;
+ var strFound;
+ if (window.find) {
+
+  // CODE FOR BROWSERS THAT SUPPORT window.find
+
+  strFound=self.find(str);
+  if (!strFound) {
+   strFound=self.find(str,0,1);
+   while (self.find(str,0,1)) continue;
+  }
+ }
+ else if (navigator.appName.indexOf("Microsoft")!=-1) {
+
+  // EXPLORER-SPECIFIC CODE
+
+  if (TRange!=null) {
+   TRange.collapse(false);
+   strFound=TRange.findText(str);
+   if (strFound) TRange.select();
+  }
+  if (TRange==null || strFound==0) {
+   TRange=self.document.body.createTextRange();
+   strFound=TRange.findText(str);
+   if (strFound) TRange.select();
+  }
+ }
+ else if (navigator.appName=="Opera") {
+  alert ("Opera browsers not supported, sorry...")
+  return;
+ }
+ if (!strFound) alert ("String '"+str+"' not found!")
+ return;
 }
