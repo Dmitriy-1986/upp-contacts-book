5b2be3d3ffd81b33861a6fda8e416caac9469776
Dovgal Dima <52016913+Dmitriy-1986@users.noreply.github.com>
2021-10-20 02:46:49 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,19 +7,3 @@ if (password == "111") {
 } else {  
       window.location.href = 'https://google.com';  
 }
-/*
-    Бургер меню
-*/
-var burgerMenu = document.getElementById('burger-menu');
-var overlay = document.getElementById('menu');
-burgerMenu.addEventListener('click',function(){
-  this.classList.toggle("close");
-  overlay.classList.toggle("overlay");
-});
-
-
-
-
-
-
-
